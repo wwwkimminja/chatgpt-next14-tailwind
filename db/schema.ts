4f9fb10c1ff8c2ba9c6b2538fbc336047d294dd2
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-11-13 22:11:32 +0900

    create table

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,49 @@
-import { pgTable,serial,text,timestamp } from "drizzle-orm/pg-core"
-export const users = pgTable('user', {
-  id: serial("id"),
-  name:text("name"),
-  email:text("email"),
-  password:text("password"),
-  role:text("role").$type<"admin"|"customer">(),
-  createAt:timestamp("created-at"),
-  updatedAt:timestamp("updated-at"),
+import { conversation } from './schema';
+import { relations } from "drizzle-orm";
+import { pgTable,text,timestamp, uuid } from "drizzle-orm/pg-core"
+export const user = pgTable('user', {
+  id: uuid("id").defaultRandom().notNull().primaryKey(),
+  name:text("name").notNull(),
+  email:text("email").notNull().unique(),
+  password:text("password").notNull(),
+  role:text("role").$type<"user"|"assistant">(),
+  createAt:timestamp("created-at").defaultNow().notNull(),
+  updatedAt:timestamp("updated-at").defaultNow().notNull(),
 });
+
+export const conversation = pgTable('conversation',{
+  id:uuid("id").defaultRandom().notNull().primaryKey(),
+  name:text("name"),
+  userId:uuid("userId").references(()=>user.id,{onDelete:'cascade'}).notNull(),
+  createAt:timestamp("created-at").defaultNow().notNull(),
+  updatedAt:timestamp("updated-at").defaultNow().notNull(),
+})
+
+export const userRelations = relations(user,({many})=>({
+  conversation:many(conversation)
+
+}))
+
+export const conversationRelations = relations(conversation,({one,many})=>({
+  user:one(user,{
+    fields:[conversation.userId],
+    references:[user.id]
+  }),
+  messages:many(message)
+}))
+
+export const message = pgTable('message',{
+  id: uuid("id").defaultRandom().notNull().primaryKey(),
+  content:text("content").notNull(),
+  role:text("role").$type<"user"|"assistant">(),
+  conversationId:uuid("conversationId").references(()=>conversation.id,{onDelete:"cascade"}).notNull(),
+  createAt:timestamp("created-at").defaultNow().notNull(),
+  updatedAt:timestamp("updated-at").defaultNow().notNull(),
+})
+
+export const messageRelations = relations(message,({one})=>({
+  conversation:one(conversation,{
+    fields:[message.conversationId],
+    references:[conversation.id]
+  })
+}))
