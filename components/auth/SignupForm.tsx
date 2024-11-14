f54f702bab7a6134ba592fa32d0c6aac5e740d30
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-11-14 18:07:20 +0900

    validation check SignUpForm

--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -1,9 +1,23 @@
+'use client';
+import { ChangeEvent } from 'react';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 import { FormCard } from './FormCard';
 import { Submit } from './Submit';
+import { useFormValidate } from '@/hooks/useFormValidate';
+import { SignUpSchema } from '@/schema/auth';
+import { TSignUpFormError } from '@/types/form';
+import { FormMessage } from './FormMessage';
 
 export function SignUpForm() {
+  const { errors, validateField } =
+    useFormValidate<TSignUpFormError>(SignUpSchema);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    validateField(name, value);
+  };
+
   return (
     <FormCard
       title="Sign Up"
@@ -13,7 +27,14 @@ export function SignUpForm() {
         {/* name */}
         <div className="space-y-1">
           <Label htmlFor="name">name</Label>
-          <Input id="name" name="name" placeholder="name" />
+          <Input
+            id="name"
+            name="name"
+            placeholder="name"
+            onChange={handleChange}
+            error={!!errors?.name}
+          />
+          {errors?.name && <FormMessage message={errors.name[0]} />}
         </div>
         {/* email */}
         <div className="space-y-1">
@@ -23,7 +44,10 @@ export function SignUpForm() {
             name="email"
             type="email"
             placeholder="example@example.com"
+            onChange={handleChange}
+            error={!!errors?.email}
           />
+          {errors?.email && <FormMessage message={errors.email[0]} />}
         </div>
         {/* password */}
         <div className="space-y-1">
@@ -33,7 +57,10 @@ export function SignUpForm() {
             name="password"
             type="password"
             placeholder="********"
+            onChange={handleChange}
+            error={!!errors?.password}
           />
+          {errors?.password && <FormMessage message={errors.password[0]} />}
         </div>
         <Submit className="w-full">Join</Submit>
       </form>
---
+++ schema/auth.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const SignUpSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'please enter your name' })
+    .regex(/^[\p{L}]+$/u, {
+      message: 'Numbers and special characters((e.g., #, %, @))cannot be used.',
+    }),
+  email: z.string().email({ message: 'The email format is incorrect.' }),
+  password: z
+    .string()
+    .min(8, { message: 'The password must be at least 8 characters long.' })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+      message:
+        'at least one uppercase letter, one lowercase letter, one number and one special character:',
+    }),
+});
