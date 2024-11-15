b6f69544fe12cf0187d01e2753d2f443c62d2f99
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-11-15 10:16:58 +0900

    add: middleware

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { AUTH_ROUTES, BASE_URL, PUBLIC_ROUTES } from './constants/routes';
+import { cookies } from 'next/headers';
+import { verify } from './actions/sessions';
+
+// この関数は、内部で`await`を使用する場合、`async`としてマークできます
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
+
+  const cookie = cookies().get('session')?.value;
+  const session = await verify(cookie)
+
+  if(!isPublicRoute && !session){
+    return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN,request.nextUrl))
+
+  }
+
+  if(isPublicRoute && session){
+    return NextResponse.redirect(new URL(BASE_URL,request.nextUrl))
+
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
