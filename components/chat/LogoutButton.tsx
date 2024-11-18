a094205437d452a86cfd96b33522a3e554d442c6
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-11-18 21:56:05 +0900

    feat logout

--- components/chat/LogoutButton.tsx
+++ components/chat/LogoutButton.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+'use client';
 import { Button } from '../ui/button';
+import { deleteSession } from '@/actions/sessions';
 
 export function LogoutButton() {
   return (
-    <Button className="w-[80%] hover:scale-105 hover:font-bold transition-all ">
+    <Button
+      className="w-[80%] hover:scale-105 hover:font-bold transition-all "
+      onClick={() => deleteSession()}
+    >
       Log out
     </Button>
   );
